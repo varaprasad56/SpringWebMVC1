84241c9ad5a495d40319c31d4d48d92ece261d9b
MVaraPrasad <varaprasad.512@gmail.com>
2021-06-15 00:01:22 +0530

    Complete Flow implemented

---
+++ src/main/java/com/vara/controller/TinyQuoteController.java
@@ -0,0 +1,14 @@
+package com.vara.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TinyQuoteController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return new ModelAndView("quote","message","Everything will be Alright");
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/quote.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: VaraPrasad
+  Date: 14-06-2021
+  Time: 23:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@page isELIgnored="false" %>
+
+<h1>Welcome to First Spring MVC Application</h1>
+<h3>${message}</h3>
+
